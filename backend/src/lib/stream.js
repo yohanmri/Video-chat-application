82e64d2a9d7928570edf6c12d4abc454124935b7
Yohan Maha Ranasingha <yohanm.ranasingha@gmail.com>
2025-09-27 04:05:08 +0530

    Completed

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -24,4 +24,12 @@ export const upsertStreamUser = async (userData) => {
 // todo :do it later
 export const generateStreamToken = (userId) => {
 
+try {
+    // ensure userId is a string
+    const userIdStr = userId.toString();
+    return streamClient.createToken(userIdStr);
+} catch (error) {
+    console.error("Error generating Stream token:", error);
 }
+
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js';  
 import userRoutes from './routes/user.route.js';
+import chatRoutes from './routes/chat.route.js';
 
 import {connectDB} from './lib/db.js';
 
@@ -28,7 +29,7 @@ app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
-// app.use('/api/chat', chatRoutes);
+app.use('/api/chat', chatRoutes);
 
 
 
